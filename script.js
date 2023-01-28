c160ef2b241125617a80b543b3eb89e10484258e
talenelat-elin <71895835+talenelat-elin@users.noreply.github.com>
2023-01-28 22:37:30 +0530

    Change targeting classnames

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         Whatsapp Web
 // @namespace    web.whatsapp.com
-// @version      1.7
+// @version      1.8
 // @match        https://web.whatsapp.com/
-// @author       You
+// @author       talenelat-elin
 // @grant        none
+// date          2023-01-28
 // ==/UserScript==
 
 document.addEventListener("keydown", function(zEvent) {
   if (zEvent.ctrlKey && zEvent.altKey && zEvent.key === "f") { // case sensitive
     // DO YOUR STUFF HERE
-    document.querySelector("._13NKt").focus();
+    document.querySelector(".Er7QU").focus();
   }
 
 
@@ -44,9 +45,8 @@ document.addEventListener("keydown", function(zEvent) {
 
   if (zEvent.altKey && zEvent.key === "ArrowUp") { // case sensitive
     // DO YOUR STUFF HERE
-    // _3K4-L is the classname for the chats container, and _1-FMR is the class name for the chat bubbles themselves.
-    document.querySelector("._33LGR > div > div:last-of-type > ._1-FMR").focus()
-
+    // _5kRIK is the classname for the chats container, and _1-FMR is the class name for the chat bubbles themselves.
+    document.querySelector("._5kRIK > div > div:last-of-type > ._1-FMR").focus()
   }
 
 
